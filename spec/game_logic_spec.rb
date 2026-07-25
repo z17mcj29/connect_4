9024075a8e7696ba11ea4e7af49b20d2c74bb6a7
Michael <z17.mcj@gmail.com>
2026-07-25 07:40:16 -0600

    working on board logic

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -55,3 +55,15 @@ describe GameLogic do
 
 
 end
+
+#I have another idea I am going to try. I will have to do some thinking to figure
+#out how to make it work. Here is what I have thought up so far and I'm writing it
+#down now because I won't be working on it until tomorrow. So here are my notes.
+#Have a for loop that runs seven times. I don't need to deal with the 8th option of
+#straight up. I will start at the location the token drops and then check each of
+#the seven directions to see if the same type of token extends three spaces out.
+#If I get three tokens that match the beginning token I have four in a row.
+#If I hit an invalid location or a different token or empty space then I will use
+#next to go to the next iteration of the for loop. If it ever does find four in a
+#row it will return true and end the check immediately. I think I can do it this
+#way. I'll work on it tomorrow.
